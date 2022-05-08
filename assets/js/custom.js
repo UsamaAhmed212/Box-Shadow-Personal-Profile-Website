89f319bc82b704b095fe19dcad6497da41cc042a
Usama Ahmed <usamaahmed212aaa@gmail.com>
2022-05-08 22:48:02 +0600

    About Section Design Complete and Typed.Js add | & and & | ( Alisandra |&&| Inheritance |&&| roboto |&&| themify-icons ) Fonts Add

---
+++ assets/js/custom.js
@@ -0,0 +1,23 @@
+// Type Js 01
+var typed = new Typed('.type_js_01', {
+    stringsElement: '.type_js_get_01',
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 3000,
+        loop: true,
+        smartBackspace: true,
+        showCursor: false
+});
+
+// Type Js 02
+var typed = new Typed('.type_js_02', {
+    stringsElement: '.type_js_get_02',
+        typeSpeed: 100,
+        backSpeed: 100,
+        startDelay: 1500,
+        backDelay: 5000,
+        loop: true,
+        smartBackspace: true,
+        showCursor: false
+});
+
